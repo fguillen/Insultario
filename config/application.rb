5ae8d4f0da5af7f7f35d73c87ed3405ad31a0b59
Fernando Guillen <fguillen.mail@gmail.com>
2018-10-07 19:39:21 +0200

    Adding :put to the CORS configuration

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Insultario
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options, :put]
       end
     end
   end
